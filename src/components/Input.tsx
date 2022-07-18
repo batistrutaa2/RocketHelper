91983e3441c5d78f5892f947d2761b986b2c8d63
batistrutaa2 <54988509+batistrutaa2@users.noreply.github.com>
2022-07-18 20:42:00 -0300

    primeira aula

---
+++ src/components/Input.tsx
@@ -0,0 +1,21 @@
+import { Input as NativeBaseinput, IInputProps } from 'native-base';
+
+export function Input({ ...rest }: IInputProps) {
+  return (
+    <NativeBaseinput
+      bg="gray.700"
+      h={14}
+      size="md"
+      borderWidth={0}
+      fontSize="md"
+      color="white"
+      placeholderTextColor="gray.300"
+      _focus={{ 
+        borderWidth: 1,
+        borderColor: "green.500",
+        bg: "gray.700"
+       }}
+      { ...rest }
+    />
+  );
+}
